47e4fce1e43cf1708fba79490a4cd9a795d96c76
Grant Hesketh <ghesketh76@gmail.com>
2020-11-11 18:48:30 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,12 +5,6 @@ require "pry"
 
 
 def load_library(path)
-  emoticons = YAML.load_file(path)
-  emoticon_lib = {}
-  emoticons.each do |key, value|
-    emoticon_lib[key][:english => value[],:japanese => value[]]
-  end
-  emoticon_lib
   
 end
 
