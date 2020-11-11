948701dd9cd0d2dd6d295359fc123f2bd799ab19
Grant Hesketh <ghesketh76@gmail.com>
2020-11-11 19:10:13 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,10 +21,9 @@ def get_english_meaning(path,emoticon)
   load_library(path).each do |key,value|
     if value[:japanese] == emoticon
       return key
-    else
-      puts "Sorry, that emoticon was not found"
-      
     end
+    
+      
     
     
   end
