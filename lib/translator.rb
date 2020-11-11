72ac5ad4cadddd6b166f7d816101593a56c46f03
Grant Hesketh <ghesketh76@gmail.com>
2020-11-11 18:24:11 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,7 +7,8 @@ require "pry"
 def load_library(path)
   # code goes here
   emoticons = {"get_meaning" => {}, "get_emoticon" => {}}
-  YAML.load_file(path) each do
+  YAML.load_file(path) each do |meaning, describe|
+    
   
  
 end
